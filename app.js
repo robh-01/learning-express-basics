4d2b6dc6b14c22b31274e475a9fadc870e19384b
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-03-21 08:12:56 +0545

    First Steps

---
+++ app.js
@@ -0,0 +1,13 @@
+//learning to setup basic server using express
+
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, world");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`My first express app - listening on port ${PORT}`);
+});
---
+++ learn_routes.js
@@ -0,0 +1,1 @@
+// learning about routes in express
